b1520eed67287d7249dcbc3f679e1992a13e650f
yyl <123@qq.com>
2018-09-14 19:18:53 +0800

    1.搭建工程环境 2.后台品牌管理 3.完成模板功能和规格功能

---
+++ pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -0,0 +1,113 @@
+package com.pinyougou.sellergoods.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.pinyougou.mapper.TbBrandMapper;
+import com.pinyougou.pojo.TbBrand;
+import com.pinyougou.pojo.TbBrandExample;
+import com.pinyougou.sellergoods.service.BrandService;
+import entity.PageResult;
+import entity.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class BrandServiceImpl implements BrandService {
+
+    @Autowired
+    private TbBrandMapper brandMapper;
+
+    /**
+     * 查询所有
+     * @return
+     */
+    @Override
+    public List<TbBrand> findAll() {
+        return brandMapper.selectByExample(null);
+}
+
+    /**
+     * 分页
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @Override
+    public PageResult findPage(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        Page<TbBrand> page = (Page<TbBrand>) brandMapper.selectByExample(null);
+        return new PageResult(page.getTotal(),page.getResult());
+    }
+
+    /**
+     * 添加
+     * @param brand
+     */
+    @Override
+    public void save(TbBrand brand) {
+        brandMapper.insert(brand);
+    }
+
+    /**
+     * 根据id查找
+     * @param id
+     * @return
+     */
+    @Override
+    public TbBrand findById(Long id) {
+        return brandMapper.selectByPrimaryKey(id);
+    }
+
+    /**
+     * 修改
+     * @param brand
+     */
+    @Override
+    public void update(TbBrand brand) {
+        brandMapper.updateByPrimaryKey(brand);
+    }
+
+    /**
+     * 删除
+     * @param ids
+     */
+    @Override
+    public void delete(Long[] ids) {
+        for (Long id : ids) {
+            brandMapper.deleteByPrimaryKey(id);
+        }
+    }
+
+    /**
+     * 条件查询
+     * @param brand
+     * @param pageNum
+     * @param pageSize
+     * @return
+     */
+    @Override
+    public PageResult findPage(TbBrand brand, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        TbBrandExample example = new TbBrandExample();
+        TbBrandExample.Criteria criteria = example.createCriteria();
+        if (brand!=null){
+            if (brand.getName()!=null&&brand.getName().length()>0){
+                criteria.andNameLike("%"+brand.getName()+"%");
+            }
+            if (brand.getFirstChar()!=null&&brand.getFirstChar().length()>0){
+                criteria.andFirstCharEqualTo(brand.getFirstChar());
+            }
+        }
+        Page<TbBrand> page = (Page<TbBrand>) brandMapper.selectByExample(example);
+        return new PageResult(page.getTotal(),page.getResult());
+    }
+
+    @Override
+    public List<Map> selectOptionList() {
+        return brandMapper.selectOptionList();
+    }
+}
